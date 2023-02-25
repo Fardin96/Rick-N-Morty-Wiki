50db5c053efc66a72235577f7c6e8e0cad876f92
Khan-17101136 <fardinshuvro96@gmail.com>
2023-02-26 00:06:49 +0600

    horizontal scroll

--- src/screens/Characters.js
+++ src/screens/Characters.js
@@ -25,10 +25,7 @@ function Characters() {
 
   return (
     <div className="App nav-padding cast-bg-image">
-      <div
-        className="container"
-        // style={{ border: "2px solid orange" }}
-      >
+      <div className="container">
         <div className="row">
           <div
             className="d-flex justify-content-between align-items-center travels-demi-bold text-blue"
